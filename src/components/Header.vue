5c7e89018de16fe004d7bda03850adff573d01bd
Keisuke Taniguchi <rememberthatk@icloud.com>
2021-04-24 15:45:05 +0900

    [master] add header

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <c-box border-bottom="2px" bg="yellow" border-color="tomato" box-shadow="lg">
+    <c-flex p="4"> Header </c-flex>
+  </c-box>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+import { CBox, CFlex } from '@chakra-ui/vue'
+
+export default defineComponent({
+  name: 'Header',
+  components: {
+    CBox,
+    CFlex,
+  },
+  setup() {},
+})
+</script>
